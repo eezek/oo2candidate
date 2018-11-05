8f6e3f85c5e56753b1fc9060fd0fef3d18fb64a1
ezequiel_costa <ezequiel_costa@dell.com>
2018-11-05 12:46:34 -0200

    CRUD

--- src/main/java/br/edu/ulbra/election/candidate/service/CandidateService.java
+++ src/main/java/br/edu/ulbra/election/candidate/service/CandidateService.java
@@ -7,9 +7,7 @@ import br.edu.ulbra.election.candidate.output.v1.GenericOutput;
 import br.edu.ulbra.election.candidate.repository.CandidateRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.modelmapper.TypeToken;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
